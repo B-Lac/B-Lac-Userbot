1c953b2d226e0c8341d2aad69058c9d06dcaaecc
Pawan JATT <76043107+jattpawan@users.noreply.github.com>
2021-04-02 08:34:33 +0530

    Update _help.py

--- userbot/plugins/_help.py
+++ userbot/plugins/_help.py
@@ -1,71 +1,50 @@
-import os
-
-from userbot import ALIVE_NAME, CMD_HELP, CMD_HNDLR, CMD_LIST
-from userbot.Config import Var
+# Rewritten by @keinshin
+import io
+from  userbot import CMD_LIST, ALIVE_NAME, bot as light
+from userbot import CMD_HELP
 from userbot.utils import admin_cmd
+import asyncio
+from var import Var
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "†hê B乛LAC Uʂҽɾ"
-CMD_HNDLR = Var.CMD_HNDLR
-CUSTOM_HELP_EMOJI = os.environ.get("CUSTOM_HELP_EMOJI", "𝔅")
-
-if CMD_HNDLR is None:
-    CMD_HNDLR = "."
-
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Pls Go To Heroku Vars Then in  `ALIVE_NAME`place You Telegram `Username` "
+    
+            
 
 @borg.on(admin_cmd(pattern="help ?(.*)"))
 async def cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
+        lightningusername = Var.TG_BOT_USER_NAME_BF_HER
         input_str = event.pattern_match.group(1)
-        if tgbotusername is None or input_str == "text":
+        if lightningusername is None or input_str == "text":
             string = ""
-            for i in CMD_HELP:
-                string += CUSTOM_HELP_EMOJI + " " + i + " " + CUSTOM_HELP_EMOJI + "\n"
-                for iter_list in CMD_HELP[i]:
+            for i in CMD_LIST:
+                string += "?對? " + i + "\n"
+                for iter_list in CMD_LIST[i]:
                     string += "    `" + str(iter_list) + "`"
                     string += "\n"
                 string += "\n"
             if len(string) > 4095:
-                with io.BytesIO(str.encode(string)) as out_file:
-                    out_file.name = "cmd.txt"
-                    await tgbot.send_file(
-                        event.chat_id,
-                        out_file,
-                        force_document=True,
-                        allow_cache=False,
-                        caption="**COMMANDS**",
-                        reply_to=reply_to_id,
-                    )
-                    await event.delete()
+                await borg.send_message(event.chat_id, "`Lol Try .help`")
+                await asyncio.sleep(5)
             else:
                 await event.edit(string)
         elif input_str:
             if input_str in CMD_LIST:
-                string = "**Commands available in {}** \n\n".format(input_str)
-                if input_str in CMD_HELP:
-                    for i in CMD_HELP[input_str]:
-                        string += i
-                    string += "\n\n**© @BLAC_USERBOT**"
-                    await event.edit(string)
-                else:
-                    for i in CMD_LIST[input_str]:
-                        string += "    " + i
-                        string += "\n"
-                    string += "\n**© @BLAC_USERBOT**"
-                    await event.edit(string)
+                string = "Commands found in {}:\n".format(input_str)
+                for i in CMD_LIST[input_str]:
+                    string += "\n    " + i
+                    string += "\n"
+                await event.edit(string)
             else:
-                await event.edit(input_str + " is not a valid plugin!")
+                await event.edit("`Wait Checking..`")
+                await asyncio.sleep(2)
+                await event.edit(input_str + "  ?對? is not a valid plugin????!")
         else:
-            help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[†hê B乛LAC Uʂҽɾზσƚ](https://github.com/B-Lac/B-Lac-Userbot)**\nIn Case The Help Dosent Pop Up do `.help <cmd name>`"""
-            try:
-                results = await bot.inline_query(  # pylint:disable=E0602
-                    tgbotusername, help_string
-                )
-                await results[0].click(
-                    event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
-                )
-                await event.delete()
-            except BaseException:
-                await event.edit(
-                    f"This bot has inline disabled. Please enable it to use `{CMD_HNDLR}help`.\nGet help from [here](t.me/blacklightningot)"
-                )
+            light_help_strin = """**Blac userbot Heres With The Detailed Help For CMDs** ???? !\n If Faced Any Bug Please Give The Feed Back at @lightningsupport:"""
+            results = await bot.inline_query(  # pylint:disable=E0602
+            lightningusername, light_help_strin
+            )
+            await results[0].click(
+                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+            )
+            await event.delete()

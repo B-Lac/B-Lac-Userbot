da6691735ef8793bd43ba00ec31339324aa75f5c
hseinkrem <83346065+hsein120@users.noreply.github.com>
2021-05-02 09:21:30 +0300

    Update awake.py

--- userbot/plugins/awake.py
+++ userbot/plugins/awake.py
@@ -7,7 +7,7 @@ from userbot.utils import admin_cmd
 
 ALIVE_PIC = Var.ALIVE_PHOTTO
 if ALIVE_PIC is None:
-    ALIVE_PIC = "https://telegra.ph/file/2f2b8d40e3f2fa4acdc8f.mp4"
+    ALIVE_PIC = "https://l.top4top.io/m_194605zdm0.mp4"
 
 
 DEFAULTUSER = (
@@ -21,13 +21,13 @@ if ALIVE_MESSAGE is None:
     ALIVE_MESSAGE += f"`Telethon: TELETHON-15.0.0 \n\n`"
     ALIVE_MESSAGE += f"`Python: PYTHON-3.8.5 \n\n`"
     ALIVE_MESSAGE += "`I'll Be With You Master Till My Dyno Ends!!☠ \n\n`"
-    ALIVE_MESSAGE += f"`Support Channel` : @blacklightningot \n\n"
+    ALIVE_MESSAGE += f"`Support Channel` : @IXKKl \n\n"
     ALIVE_MESSAGE += f"`MY BOSS🤗`: {DEFAULTUSER} \n\n "
 
 
-# @command(outgoing=True, pattern="^.awake$")
-@borg.on(admin_cmd(pattern=r"awake"))
-async def amireallyalive(awake):
+# @command(outgoing=True, pattern="^.h$")
+@borg.on(admin_cmd(pattern=r"h"))
+async def amireallyalive(h):
     """For .awake command, check if the bot is running."""
     await awake.delete()
     await borg.send_file(awake.chat_id, ALIVE_PIC, caption=ALIVE_MESSAGE)

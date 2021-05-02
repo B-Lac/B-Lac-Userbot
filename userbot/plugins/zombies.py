8065e04d8159bc8390d801c5be6795f84bcd15b0
hseinkrem <83346065+hsein120@users.noreply.github.com>
2021-05-02 08:55:42 +0300

    Update zombies.py

--- userbot/plugins/zombies.py
+++ userbot/plugins/zombies.py
@@ -5,7 +5,7 @@
 """Cmd= `.zombie`
 Usage: Searches for deleted accounts in a groups and channels.
 Use .zombies clean to remove deleted accounts from the groups and channels.
-\nPorted by ©[NIKITA](t.me/kirito6969) and ©[EYEPATCH](t.me/NeoMatrix90)"""
+\nPorted by ©[NIKITA](https://t.me/IXKKl) and ©[EYEPATCH](https://t.me/IXKKl)"""
 
 #
 from asyncio import sleep

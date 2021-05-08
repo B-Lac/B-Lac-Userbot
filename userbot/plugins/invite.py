eb3ef6d7362a3731685aeb5967ec1bc541130d5f
W2H Galaxy <82048990+W2HGalaxy-OP@users.noreply.github.com>
2021-05-08 14:57:04 +0530

    Update invite.py

--- userbot/plugins/invite.py
+++ userbot/plugins/invite.py
@@ -1,24 +1,24 @@
 # Originally made by @rekcah for @javes05
-# porting to hellbot by @kraken_the_badass...
+# porting to hellbot by @David99q...
 # i asked rekcah before porting...not like other kangers....
 # keep credit if u wanna kang...
+# Now in W2HBOT
 # else u are a gay...no doubt in that....
 
 # --------------------------------------------------------------------------------------------------------------------------------
 
-import os
-
-from userbot import *
-from userbot import CMD_HELP
-from userbot.utils import admin_cmd
-
-FULL_SUDO = os.environ.get("FULL_SUDO", None)
+from telethon.errors import (
+    ChannelInvalidError,
+    ChannelPrivateError,
+    ChannelPublicGroupNaError,
+)
 from telethon.tl import functions
 from telethon.tl.functions.channels import GetFullChannelRequest
 from telethon.tl.functions.messages import GetFullChatRequest
 
 from userbot import CMD_HELP
-
+from W2HBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+from userbot.cmdhelp import CmdHelp
 
 async def get_chatinfo(event):
     chat = event.pattern_match.group(1)
@@ -64,47 +64,47 @@ def user_full_name(user):
     return full_name
 
 
-@borg.on(admin_cmd(pattern="inviteall ?(.*)"))
-@borg.on(sudo_cmd(pattern="inviteall ?(.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern="inviteall ?(.*)"))
+@bot.on(sudo_cmd(pattern="inviteall ?(.*)", allow_sudo=True))
 async def get_users(event):
     sender = await event.get_sender()
     me = await event.client.get_me()
     if not sender.id == me.id:
-        hell = await event.reply("`processing...`")
+        W2H = await edit_or_reply(event, "`processing...`")
     else:
-        hell = await event.edit("`processing...`")
-    kraken = await get_chatinfo(event)
+        W2H = await edit_or_reply(event, "`processing...`")
+    aura = await get_chatinfo(event)
     chat = await event.get_chat()
     if event.is_private:
-        return await hell.edit("`Sorry, Can add users here`")
+        return await W2H.edit("`Sorry, Cant add users here`")
     s = 0
     f = 0
     error = "None"
 
-    await hell.edit("**TerminalStatus**\n\n`Collecting Users.......`")
-    async for user in event.client.iter_participants(kraken.full_chat.id):
+    await W2H.edit("**TerminalStatus**\n\n`Collecting Users.......`")
+    async for user in event.client.iter_participants(aura.full_chat.id):
         try:
             if error.startswith("Too"):
-                return await hell.edit(
+                return await W2H.edit(
                     f"**Terminal Finished With Error**\n(`May Got Limit Error from telethon Please try agin Later`)\n**Error** : \n`{error}`\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people"
                 )
             await event.client(
                 functions.channels.InviteToChannelRequest(channel=chat, users=[user.id])
             )
             s = s + 1
-            await hell.edit(
+            await W2H.edit(
                 f"**Terminal Running...**\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people\n\n**× LastError:** `{error}`"
             )
         except Exception as e:
             error = str(e)
             f = f + 1
-    return await hell.edit(
+    return await W2H.edit(
         f"**Terminal Finished** \n\n• Successfully Invited `{s}` people \n• failed to invite `{f}` people"
     )
 
 
-@borg.on(admin_cmd(pattern="add ?(.*)"))
-@borg.on(sudo_cmd(pattern="add ?(.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern="add ?(.*)"))
+@bot.on(sudo_cmd(pattern="add ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -139,13 +139,8 @@ async def _(event):
             await edit_or_reply(event, "Added user to the chat....")
 
 
-CMD_HELP.update(
-    {
-        "invite": """**Plugin : **`invite`
-  •  **Syntax : **`.add username(s)/userid(s)`
-  •  **Function : **__Add the given user/users to the group where u used the command__
-  •  **Syntax : **`.inviteall groups username`
-  •  **Function : **__Scrapes users from the given chat to your group__
-"""
-    }
-)
+CmdHelp("invite").add_command(
+  "add", "<username/id>", "Adds the given user to the group"
+).add_command(
+  "inviteall", "<group username>", "Scraps user from the targeted group to your group. Basically Kidnapps user from one chat to another"
+).add()
